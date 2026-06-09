ff97457c029d1a119639395e284c5745786c0bc6
zannykupdate <cottogns@hotmail.com>
2026-06-09 03:33:46 -0700

    Delete test_http.js

--- test_http.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-http.get('http://127.0.0.1:3000/api/test_error', (resp) => {
-  let data = '';
-  resp.on('data', (chunk) => { data += chunk; });
-  resp.on('end', () => { console.log(data); });
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
-});
